1799d37de4a13d143849dd90c23e3611c7dae063
elogada <bayanielogada@gmail.com>
2025-10-18 05:15:37 +0800

    vision accuracy, added models

--- start-vision.bat
+++ start-vision.bat
@@ -3,4 +3,4 @@ echo WELCOME TO ASTRAMECH VISION CHECKER
 echo Vision Checker v1.0 (C) Bayani Elogada
 echo Starting up vision script...
 cd c:/astramech
-python c:/astramech/vision-check.py --latest-file vision-latest.json --topk 2 --print-rate 1 --use-all-models --conf 0.78
+python c:/astramech/vision-check.py --latest-file vision-latest.json --topk 2 --print-rate 1 --use-all-models --conf 0.55
